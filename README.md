f61ba01fb9f283a5f25b33a08b06ef61628b0a69
Evan <evandvance@gmail.com>
2024-03-19 09:31:18 -0500

    Added files for encryption and function to encrypt all files in the directory

--- README.md
+++ README.md
@@ -5,8 +5,38 @@ the SPR24 CYB440 Test 2
 
 ## Requirements
 
-- Encrypt and Decrypt Files
-- Make recreatable work
-- Hash Files
-- Create HMACs for files
-- Derive a hexkey from a passphrase
+### Task 1
+
+Create an explination of the advantages/disadvanteges of symmetric encryption
+
+`Explination Here`
+
+### Task 2
+
+Provide an explanation of what bit-level you plan to utilize within your solution
+and why it is the recommended NIST level. Provide insite into the minimum recommended bit-levels.
+
+### Task 3
+
+Create a way to create a 256-bit or 32-byte key from a passphrase for encryption. Use the password "cryptogrophy"
+
+### Task 4
+
+Create a way to encrypt and decrypt files to protext confidentiality.
+
+### Task 5
+
+Create a method for the user to create a MAC/HMAC for each file before encryption and after decryption
+
+### Task 6
+
+Create a solution that will allow the users to exchange messages or files but also include a MAC to provide an
+extra layer of confidence in the authenticity of the file. Use the password "cryptography"
+
+## How to Use this Code
+
+Clone this repo then run:
+
+`python3 main.py`
+
+The terminal will walk you through all your processes and tell what has occured.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import subprocess
 from shutil import which
+import glob
 
+MASTERPASSWORD:str = "cryptography"
 
 def is_tool(name:str) -> bool:
     """A function to check if a command line tool is installed
@@ -70,7 +72,7 @@ def load_key() -> str:
 #TODO ADD support for hashing files and creating MACs
 
 
-def encrypt(input_file:str, output_file:str, key:str) -> None:
+def encrypt_file(input_file:str, output_file:str, key:str) -> None:
     """Encrypts a file using aes-256-cbc and a key
 
     Args:
@@ -79,6 +81,7 @@ def encrypt(input_file:str, output_file:str, key:str) -> None:
         key (str): Key for encryption
     """
     run_command(["openssl", "enc", "-aes-256-cbc", "-pbkdf2", "-pass", f"pass:{key}", "-in", input_file, "-out", output_file])
+    print(f"\nThe file: {input_file} has been encrypted at destination {output_file} with the key {key}")
 
 
 def decrypt_file(input_file:str, output_file:str, key:str) -> None:
@@ -90,6 +93,32 @@ def decrypt_file(input_file:str, output_file:str, key:str) -> None:
         key (str): Key for decryption
     """
     run_command(["openssl", "enc", "-d", "-aes-256-cbc", "-pbkdf2", "-pass", f"pass:{key}", "-in", input_file, "-out", output_file])
+    print(f"\nThe file: {input_file} has been decrypted at destination {output_file} with the key {key}")
+
+
+
+def encrypt_all(key:str) -> None:
+    """A function to encrypt every file in the plaintext folder
+
+    Args:
+        key (str): The key used for encryption
+    """
+    files = [file for file in glob.glob("./files/plaintext/*.txt")]
+
+    for file in files:
+        encrypt_file(file, f"./files/encrypted/{file.replace("./files/plaintext/","")}",key)
+
+
+def decrypt_all(key:str) -> None:
+    """A function to decrypt every file in the encrypted folder using a key passed to it
+
+    Args:
+        key (str): The key used to encrypt the files
+    """
+    files = [file for file in glob.glob("./files/encrypted/*.txt")]
+
+    for file in files:
+        decrypt_file(file, f"./files/decrypted/{file.replace("./files/encrypted/","")}", key)
 
 
 def main():
@@ -110,7 +139,7 @@ def main():
         elif selector == "2":
             passphrase = input("Enter your passphrase: ")
 
-            key = generate_key(passphrase)
+            key = generate_key(passphrase or MASTERPASSWORD)
 
             print(f"Your key is {key}")
 
@@ -130,23 +159,29 @@ def main():
 
         if selector == "1":
 
-            filename = input("\nEnter file name in files plaintext folder: ")
-            path = f"./files/plaintext/{filename}"
-            out_path = f"./files/encrypted/{filename}.enc"
+            filename = input("\nEnter file name in files plaintext folder or press enter to do them all: ")
 
-            encrypt(path, key, out_path)
+            if filename == "":
+                encrypt_all(key)
+            else:
+                path = f"./files/plaintext/{filename}"
+                out_path = f"./files/encrypted/{filename}.enc"
 
-            print(f"\nThe file: {filename} has been encrypted at destination {out_path} with the key {key}")
+                encrypt_file(path, key, out_path)
+
 
         elif selector == "2":
 
-            filename = input("Enter file name in files encrypted folder: ")
-            path = f"./files/encrypted/{filename}"
-            out_path = f"./files/decrypted/{filename}.txt"
+            filename = input("Enter file name in files encrypted folder or press enter to do all: ")
 
-            print(f"\nThe file: {filename} has been decrypted at destination {out_path} with the key {key}")
+            if filename == "":
+                decrypt_all(key)
 
-            encrypt(path, key, out_path)
+            else:
+                path = f"./files/encrypted/{filename}"
+                out_path = f"./files/decrypted/{filename}.txt"
+
+                decrypt_file(path, key, out_path)
 
         elif selector == "3":
             exit()
